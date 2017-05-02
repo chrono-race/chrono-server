4b704d6a54514a10e51d7c751065aa740a0a4ffd
David Green <dave@activelylazy.co.uk>
2017-05-02 17:39:28 +0100

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-var app = require('express')();  
-var server = require('http').Server(app);  
-var io = require('socket.io')(server);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 let listeners = [];
 
 function tick() {
   console.log('tick!');
-  listeners.forEach(socket => {
+  listeners.forEach((socket) => {
     socket.emit('announcements', { message: 'tick!' });
-  })
+  });
 }
 
 function onDisconnect(socket) {
@@ -23,12 +23,12 @@ function onConnect(socket) {
   socket.emit('announcements', { message: 'A new user has joined!' });
 }
 
-app.get('/', function(req, res) {  
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on('connection', onConnect);
 
 console.log('Starting server on port 8000');
 setInterval(tick, 1000);
-server.listen(8000); 
+server.listen(8000);
