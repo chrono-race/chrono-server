de1467830726cd7d7141fd688ee842722be27532
David Green <dave@activelylazy.co.uk>
2017-05-05 08:23:20 +0100

    Poller should fetch current

---
+++ test/data_source/poller_test.js
@@ -0,0 +1,32 @@
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { assert, should } from 'chai';
+import sinon from 'sinon';
+import currFetcher from '../../src/data_source/network/curr_fetcher';
+import poller from '../../src/data_source/poller';
+
+should();
+
+describe('poller', () => {
+  let fetchCurrent;
+
+  beforeEach(() => {
+    fetchCurrent = sinon.stub(currFetcher, 'fetch');
+  });
+
+  afterEach(() => {
+    fetchCurrent.restore();
+  });
+
+  it('fetches current', () => {
+    const baseUrl = 'http://localhost:8080';
+    const startTime = 1000;
+    const archiver = { };
+
+    const p = poller.create(baseUrl, archiver, startTime);
+
+    p.poll();
+
+    assert(fetchCurrent.calledOnce);
+    assert(fetchCurrent.calledWith(baseUrl, startTime + 1, archiver));
+  });
+});
