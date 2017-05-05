4907f286d1812c25e542b5ea7b145dfd3a46ec8c
David Green <dave@activelylazy.co.uk>
2017-05-05 08:19:14 +0100

    Refactor to be test of just timing logic

--- test/data_source/polling_timer_test.js
+++ test/data_source/polling_timer_test.js
@@ -2,46 +2,61 @@ import { describe, it, before, after } from 'mocha';
 import { assert, should } from 'chai';
 import sinon from 'sinon';
 import winston from 'winston';
-import currFetcher from '../../src/data_source/network/curr_fetcher';
 import dataPoller from '../../src/data_source/polling_timer';
+import poller from '../../src/data_source/poller';
 
 should();
 
 describe('data poller', () => {
   describe('start', () => {
     let clock;
-    let fetch;
     let info;
+    let createPoller;
 
     before(() => {
       clock = sinon.useFakeTimers();
-      fetch = sinon.stub(currFetcher, 'fetch');
       info = sinon.stub(winston, 'info');
+      createPoller = sinon.stub(poller, 'create');
     });
 
     after(() => {
       clock.restore();
-      fetch.restore();
       info.restore();
+      createPoller.restore();
     });
 
-    it('should poll for cur.js once a second', () => {
+    it('should create a new poller', () => {
       const baseUrl = 'http://localhost:8080/';
       const archiver = { };
       const startTime = 123456;
+      const p = { poll: () => { } };
 
-      dataPoller.start(baseUrl, archiver, startTime, currFetcher);
+      createPoller.returns(p);
+
+      dataPoller.start(baseUrl, archiver, startTime);
+
+      assert(createPoller.calledOnce);
+      assert(createPoller.calledWith(baseUrl, archiver, startTime));
+    });
+
+    it('should poll once a second', () => {
+      const baseUrl = 'http://localhost:8080/';
+      const archiver = { };
+      const startTime = 123456;
+      const p = { poll: () => { } };
+      const poll = sinon.stub(p, 'poll');
+
+      createPoller.returns(p);
+
+      dataPoller.start(baseUrl, archiver, startTime);
 
       clock.tick(1000);
 
-      assert(fetch.calledOnce);
-      assert(fetch.calledWith(baseUrl, 123457, archiver));
+      assert(poll.calledOnce);
 
       clock.tick(1000);
 
-      assert(fetch.calledTwice);
-      assert(fetch.calledWith(baseUrl, 123458, archiver));
-      assert(info.calledWith('poller started'));
+      assert(poll.calledTwice);
     });
   });
 });
