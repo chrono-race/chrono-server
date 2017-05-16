c9b4e9c7ffe50c213ea55ee8fdea1acaf1d8a080
David Green <dave@activelylazy.co.uk>
2017-05-16 18:17:20 +0100

    Catch errors and reject

--- src/data_source/network/fetcher.js
+++ src/data_source/network/fetcher.js
@@ -5,25 +5,29 @@ const UserAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTM
 
 function fetch(baseUrl, filename, archiver) {
   return new Promise((fulfill, reject) => {
-    const options = {
-      url: `${baseUrl}${filename}`,
-      headers: {
-        'User-Agent': UserAgent,
-      },
-    };
-    request.get(options, (error, response, body) => {
-      archiver.logFile(filename, body);
-      if (error != null) {
-        winston.error(`error fetching ${options.url}: ${error.message}`);
-        reject(error);
-      } else if (response.statusCode === 200) {
-        winston.info(`successfully fetched ${options.url}`);
-        fulfill(body);
-      } else {
-        winston.error(`error fetching ${options.url} - status code ${response.statusCode}`);
-        reject(new Error(`Unexpected status code: ${response.statusCode}`));
-      }
-    });
+    try {
+      const options = {
+        url: `${baseUrl}${filename}`,
+        headers: {
+          'User-Agent': UserAgent,
+        },
+      };
+      request.get(options, (error, response, body) => {
+        archiver.logFile(filename, body);
+        if (error != null) {
+          winston.error(`error fetching ${options.url}: ${error.message}`);
+          reject(error);
+        } else if (response.statusCode === 200) {
+          winston.info(`successfully fetched ${options.url}`);
+          fulfill(body);
+        } else {
+          winston.error(`error fetching ${options.url} - status code ${response.statusCode}`);
+          reject(new Error(`Unexpected status code: ${response.statusCode}`));
+        }
+      });
+    } catch (e) {
+      reject(e);
+    }
   });
 }
 
