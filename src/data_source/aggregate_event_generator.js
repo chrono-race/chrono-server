991933df1091528afc724948ba0e2cd9546ee3c5
David Green <dave@activelylazy.co.uk>
2017-06-07 17:54:38 +0100

    Add time of day events into aggregate event generator

--- src/data_source/aggregate_event_generator.js
+++ src/data_source/aggregate_event_generator.js
@@ -2,17 +2,22 @@ import driverParser from './page1/driver_parser';
 import gapsParser from './page1/gaps_parser';
 import page1Parser from './page1/parser';
 import eventGeneratorFactory from './page1/event_generator';
+import timeOfDayParser from './time_of_day/parser';
+import timeOfDayEventGeneratorFactory from './time_of_day/event_generator';
 
 function startSession(allJson, eventPublisher) {
   const drivers = driverParser.extractDrivers(allJson);
   const page1EventGenerator = eventGeneratorFactory.initialise();
+  const timeOfDayEventGenerator = timeOfDayEventGeneratorFactory.initialise();
   return {
     update: (curJson) => {
       const gaps = gapsParser.parse(drivers, curJson);
       const page1 = page1Parser.parse(drivers, curJson);
+      const timeOfDay = timeOfDayParser.parse(curJson);
 
       const events = page1EventGenerator.generateFrom(gaps, page1);
-      eventPublisher(events);
+      const timeOfDayEvents = timeOfDayEventGenerator.generateFrom(timeOfDay);
+      eventPublisher(events.concat(timeOfDayEvents));
     },
   };
 }
--- test/data_source/aggregate_event_generator_test.js
+++ test/data_source/aggregate_event_generator_test.js
@@ -6,6 +6,8 @@ import eventGenerator from '../../src/data_source/page1/event_generator';
 import gapsParser from '../../src/data_source/page1/gaps_parser';
 import page1Parser from '../../src/data_source/page1/parser';
 import { startSession } from '../../src/data_source/aggregate_event_generator';
+import timeOfDayParser from '../../src/data_source/time_of_day/parser';
+import timeOfDayEventGeneratorFactory from '../../src/data_source/time_of_day/event_generator';
 
 should();
 
@@ -40,41 +42,56 @@ describe('lap message generator', () => {
   describe('on update', () => {
     let parseGaps;
     let parsePage1;
+    let parseTimeOfDay;
+    let timeOfDayFactory;
 
     beforeEach(() => {
       parseGaps = sinon.stub(gapsParser, 'parse');
       parsePage1 = sinon.stub(page1Parser, 'parse');
+      // console.log(`stubbing in ${timeOfDayEventGeneratorFactory.initialise}`);
+      // console.log(`stubbing in ${timeOfDayParser.parse}`);
+      parseTimeOfDay = sinon.stub(timeOfDayParser, 'parse');
+      timeOfDayFactory = sinon.stub(timeOfDayEventGeneratorFactory, 'initialise');
     });
 
     afterEach(() => {
       parseGaps.restore();
       parsePage1.restore();
+      parseTimeOfDay.restore();
+      timeOfDayFactory.restore();
     });
 
-    it('parses page 1 gaps & drivers, generates events then publishes', () => {
+    it('parses, generates events then publishes', () => {
       const allJson = { all: 'json' };
       const drivers = [{ driver: 'VAN' }];
-      const generator = { generateFrom: () => { } };
+      const page1EventGenerator = { generateFrom: () => { } };
+      const timeOfDayEventGenerator = { generateFrom: () => { } };
       const curJson = { current: 'json' };
       const gaps = { gaps: [] };
       const page1 = { page: 'one' };
       const events = [{ event: 1 }];
+      const timeOfDay = { time: 1234 };
+      const timeOfDayEvents = [{ event: 2 }];
 
-      const updateGenerator = sinon.stub(generator, 'generateFrom');
+      const updateGenerator = sinon.stub(page1EventGenerator, 'generateFrom');
+      const timeOfDayGenerator = sinon.stub(timeOfDayEventGenerator, 'generateFrom');
       const publisher = sinon.stub();
 
       extractDrivers.withArgs(allJson).returns(drivers);
-      initialise.returns(generator);
+      initialise.returns(page1EventGenerator);
+      timeOfDayFactory.returns(timeOfDayEventGenerator);
 
       parseGaps.withArgs(drivers, curJson).returns(gaps);
       parsePage1.withArgs(drivers, curJson).returns(page1);
+      parseTimeOfDay.withArgs(curJson).returns(timeOfDay);
 
       updateGenerator.withArgs(gaps, page1).returns(events);
+      timeOfDayGenerator.withArgs(timeOfDay).returns(timeOfDayEvents);
 
       const session = startSession(allJson, publisher);
       session.update(curJson);
 
-      assert(publisher.calledWith(events));
+      assert(publisher.calledWith([{ event: 1 }, { event: 2 }]));
     });
   });
 });
