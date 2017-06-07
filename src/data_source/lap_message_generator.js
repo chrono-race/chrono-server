e94b7773bee0a7c549fbd5c3dacc8a340398c71c
David Green <dave@activelylazy.co.uk>
2017-06-07 08:06:09 +0100

    Rename as factory & page1EventGenerator

--- src/data_source/lap_message_generator.js
+++ src/data_source/lap_message_generator.js
@@ -1,17 +1,17 @@
 import driverParser from './page1/driver_parser';
 import gapsParser from './page1/gaps_parser';
 import page1Parser from './page1/parser';
-import eventGenerator from './page1/event_generator';
+import eventGeneratorFactory from './page1/event_generator';
 
 function startSession(allJson, eventPublisher) {
   const drivers = driverParser.extractDrivers(allJson);
-  const generator = eventGenerator.initialise();
+  const page1EventGenerator = eventGeneratorFactory.initialise();
   return {
     update: (curJson) => {
       const gaps = gapsParser.parse(drivers, curJson);
       const page1 = page1Parser.parse(drivers, curJson);
 
-      const events = generator.updateWith(gaps, page1);
+      const events = page1EventGenerator.updateWith(gaps, page1);
       eventPublisher(events);
     },
   };
