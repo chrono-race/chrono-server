ce5e033137d2e8f3f353e697bdec91a37970f6e5
David Green <dave@activelylazy.co.uk>
2017-06-09 17:16:51 +0100

    Add race control messages to aggregate event generator

--- src/data_source/aggregate_event_generator.js
+++ src/data_source/aggregate_event_generator.js
@@ -4,20 +4,25 @@ import page1Parser from './page1/parser';
 import eventGeneratorFactory from './page1/event_generator';
 import timeOfDayParser from './time_of_day/parser';
 import timeOfDayEventGeneratorFactory from './time_of_day/event_generator';
+import raceControlMessageParser from './race_control_message/parser';
+import raceControlMessageEventGeneratorFactory from './race_control_message/event_generator';
 
 function startSession(allJson, eventPublisher) {
   const drivers = driverParser.extractDrivers(allJson);
   const page1EventGenerator = eventGeneratorFactory.initialise();
   const timeOfDayEventGenerator = timeOfDayEventGeneratorFactory.initialise();
+  const raceControlMessageEventGenerator = raceControlMessageEventGeneratorFactory.initialise();
   return {
     update: (curJson) => {
       const gaps = gapsParser.parse(drivers, curJson);
       const page1 = page1Parser.parse(drivers, curJson);
       const timeOfDay = timeOfDayParser.parse(curJson);
+      const raceControlMessage = raceControlMessageParser.parse(curJson);
 
       const events = page1EventGenerator(gaps, page1);
       const timeOfDayEvents = timeOfDayEventGenerator(timeOfDay);
-      eventPublisher(events.concat(timeOfDayEvents));
+      const raceControlMessageEvents = raceControlMessageEventGenerator(raceControlMessage);
+      eventPublisher(events.concat(timeOfDayEvents).concat(raceControlMessageEvents));
     },
   };
 }
--- test/data_source/aggregate_event_generator_test.js
+++ test/data_source/aggregate_event_generator_test.js
@@ -8,6 +8,8 @@ import page1Parser from '../../src/data_source/page1/parser';
 import { startSession } from '../../src/data_source/aggregate_event_generator';
 import timeOfDayParser from '../../src/data_source/time_of_day/parser';
 import timeOfDayEventGeneratorFactory from '../../src/data_source/time_of_day/event_generator';
+import raceControlMessageParser from '../../src/data_source/race_control_message/parser';
+import raceControlMessageEventGeneratorFactory from '../../src/data_source/race_control_message/event_generator';
 
 should();
 
@@ -43,22 +45,26 @@ describe('lap message generator', () => {
     let parseGaps;
     let parsePage1;
     let parseTimeOfDay;
+    let parseRaceControlMessage;
     let timeOfDayFactory;
+    let raceControlMessageFactory;
 
     beforeEach(() => {
       parseGaps = sinon.stub(gapsParser, 'parse');
       parsePage1 = sinon.stub(page1Parser, 'parse');
-      // console.log(`stubbing in ${timeOfDayEventGeneratorFactory.initialise}`);
-      // console.log(`stubbing in ${timeOfDayParser.parse}`);
       parseTimeOfDay = sinon.stub(timeOfDayParser, 'parse');
+      parseRaceControlMessage = sinon.stub(raceControlMessageParser, 'parse');
       timeOfDayFactory = sinon.stub(timeOfDayEventGeneratorFactory, 'initialise');
+      raceControlMessageFactory = sinon.stub(raceControlMessageEventGeneratorFactory, 'initialise');
     });
 
     afterEach(() => {
       parseGaps.restore();
       parsePage1.restore();
       parseTimeOfDay.restore();
+      parseRaceControlMessage.restore();
       timeOfDayFactory.restore();
+      raceControlMessageFactory.restore();
     });
 
     it('parses, generates events then publishes', () => {
@@ -66,30 +72,36 @@ describe('lap message generator', () => {
       const drivers = [{ driver: 'VAN' }];
       const page1EventGenerator = sinon.stub();
       const timeOfDayEventGenerator = sinon.stub();
+      const raceControlMessageGenerator = sinon.stub();
       const curJson = { current: 'json' };
       const gaps = { gaps: [] };
       const page1 = { page: 'one' };
       const events = [{ event: 1 }];
       const timeOfDay = { time: 1234 };
       const timeOfDayEvents = [{ event: 2 }];
+      const raceControlMessage = { message: 'test' };
+      const raceControlMessageEvents = [{ event: 3 }];
 
       const publisher = sinon.stub();
 
       extractDrivers.withArgs(allJson).returns(drivers);
       initialise.returns(page1EventGenerator);
       timeOfDayFactory.returns(timeOfDayEventGenerator);
+      raceControlMessageFactory.returns(raceControlMessageGenerator);
 
       parseGaps.withArgs(drivers, curJson).returns(gaps);
       parsePage1.withArgs(drivers, curJson).returns(page1);
       parseTimeOfDay.withArgs(curJson).returns(timeOfDay);
+      parseRaceControlMessage.withArgs(curJson).returns(raceControlMessage);
 
       page1EventGenerator.withArgs(gaps, page1).returns(events);
       timeOfDayEventGenerator.withArgs(timeOfDay).returns(timeOfDayEvents);
+      raceControlMessageGenerator.withArgs(raceControlMessage).returns(raceControlMessageEvents);
 
       const session = startSession(allJson, publisher);
       session.update(curJson);
 
-      assert(publisher.calledWith([{ event: 1 }, { event: 2 }]));
+      assert(publisher.calledWith([{ event: 1 }, { event: 2 }, { event: 3 }]));
     });
   });
 });
