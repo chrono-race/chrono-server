856cf0358bb7bddcde10031df0f8435520a11716
David Green <dave@activelylazy.co.uk>
2017-06-13 17:52:50 +0100

    Pit message parser should error in case number of drivers disagrees

--- src/data_source/pit_message/parser.js
+++ src/data_source/pit_message/parser.js
@@ -1,6 +1,15 @@
 import findSessionBlock from '../page1/find_session_block';
 
-function parse(input) {
+function parse(drivers, input) {
+  if (input.x === undefined) {
+    return null;
+  }
+
+  const driversPitData = input.x[findSessionBlock.from(input.x)].DR;
+
+  if (driversPitData.length !== drivers.length) {
+    throw new Error(`Expected ${drivers.length} drivers in x block but found ${driversPitData.length}`);
+  }
   return null;
 }
 
--- test/data_source/pit_message/parser_test.js
+++ test/data_source/pit_message/parser_test.js
@@ -5,11 +5,37 @@ import pitMessageParser from '../../../src/data_source/pit_message/parser';
 should();
 
 describe('pit message parser', () => {
+  const drivers = [
+    {
+      tla: 'VAN',
+    },
+    {
+      tla: 'ALO',
+    },
+    {
+      tla: 'VET',
+    },
+  ];
+
   it('should return null in case of no x data block', () => {
     const input = { };
 
-    const pitData = pitMessageParser.parse(input);
+    const pitData = pitMessageParser.parse(drivers, input);
 
     assert.isNull(pitData);
+  });
+
+  it('errors in case size of driver list disagrees', () => {
+    const input = {
+      x: {
+        Test_Race_1234: {
+          DR: [
+            { }, { },
+          ],
+        },
+      },
+    };
+
+    assert.throws(() => pitMessageParser.parse(drivers, input), 'Expected 3 drivers in x block but found 2');
   });
 });
