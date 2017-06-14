408a5f056c3242d40b6501642fefb967fb0dc3e0
David Green <dave@activelylazy.co.uk>
2017-06-14 17:45:52 +0100

    Outputs message for a driver on receipt of pit data

--- src/data_source/pit_message/event_generator.js
+++ src/data_source/pit_message/event_generator.js
@@ -1,6 +1,18 @@
 
+function createDriverPitMessage(driver, driverPitData) {
+  return Object.assign({
+    type: 'pit',
+    driver,
+  }, driverPitData);
+}
+
 function initialise() {
-  return (pitData) => [];
+  return (pitData) => {
+    if (pitData === null) {
+      return [];
+    }
+    return Object.keys(pitData).map(driver => createDriverPitMessage(driver, pitData[driver]));
+  };
 }
 
 module.exports = { initialise };
--- test/data_source/pit_message/event_generator_test.js
+++ test/data_source/pit_message/event_generator_test.js
@@ -13,4 +13,30 @@ describe('page 1 event generator', () => {
 
     assert(events.length.should.equal(0));
   });
+
+  it('outputs message for a driver on receipt of pit data', () => {
+    const eventGenerator = eventGeneratorFactory.initialise();
+
+    const pitData = {
+      VET: {
+        currentStatus: '',
+        stints: [
+          {
+            startLap: 1,
+            tyre: 'S',
+          },
+        ],
+      },
+    };
+
+    const events = eventGenerator(pitData);
+
+    assert(events.length.should.equal(1));
+    assert(events[0].type.should.equal('pit'));
+    assert(events[0].driver.should.equal('VET'));
+    assert(events[0].currentStatus.should.equal(''));
+    assert(events[0].stints.length.should.equal(1));
+    assert(events[0].stints[0].startLap.should.equal(1));
+    assert(events[0].stints[0].tyre.should.equal('S'));
+  });
 });
